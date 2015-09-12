2eee70fc8785cfbb17487d0fa65db220ae45e00e
Harshvardhan Sharma <harsh1618@outlook.com>
2015-09-13 00:31:44 +0530

    added function to fetch API response

--- popup.js
+++ popup.js
@@ -27,9 +27,55 @@
 //  });
 //});
 
+var token = '';
+var tokenId = '';
+
+var productName = document.getElementsByTagName('h1')[0].innerHTML;
+var productCategory = getElementByXpath('//*[@id="fk-mainbody-id"]/div/div[2]/div/div/div/ul/li[2]/a').innerHTML;
+
 function getElementByXpath(path) {
       return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-var productName = document.getElementsByTagName('h1')[0].innerHTML;
-var productCategory = getElementByXpath('//*[@id="fk-mainbody-id"]/div/div[2]/div/div/div/ul/li[2]/a').innerHTML;
+function getApiResponse(apiName, apiParams, token, tokenId) {
+    var apiReq = new XMLHttpRequest();
+    apiReq.open("GET", "https://api.quikr.com" + apiName + apiParams, false);
+    apiReq.setRequestHeader("X-Quikr-App-Id", "541");
+    apiReq.setRequestHeader("X-Quikr-Token-Id", tokenId);
+
+    var data = "541" + apiName + "2015-09-13";
+    var signature = CryptoJS.HmacSHA1(data, token).toString();
+
+    apiReq.setRequestHeader("X-Quikr-Signature", signature);
+    apiReq.send();
+    return apiReq.response;
+}
+
+function handleTokenResponse() {
+    var response = JSON.parse(this.response);
+    token = response['token'];
+    tokenId = response['tokenId'];
+    console.log(token);
+    console.log(tokenId);
+    var apiName = '/public/adsByCategory'; 
+    var apiParams = '?categoryId=149&from=0&size=20';
+    console.log(getApiResponse(apiName, apiParams, token, tokenId));
+}
+
+var secret = "0c2487f6740c0bf89b64a4827e07cbe2";
+
+var data = "mohitashokgarg@gmail.com" + "541" + "2015-09-13";
+var signature = CryptoJS.HmacSHA1(data, secret).toString();
+var tokenReqJson = {"appId" : "541", "signature" : signature};
+
+var tokenReq = new XMLHttpRequest();
+tokenReq.open("POST", "https://api.quikr.com/app/auth/access_token", true);
+tokenReq.addEventListener("load", handleTokenResponse);
+tokenReq.setRequestHeader("Content-Type", "application/json");
+tokenReq.send(JSON.stringify(tokenReqJson));
+
+//var categoryReq = new XMLHttpRequest();
+//categoryReq.open("GET", "https://api.quikr.com/public/adsByCategory?categoryId=149&from=0&size=20", true);
+//categoryReq.send();
+//console.log(categoryReq.responseText);
+
