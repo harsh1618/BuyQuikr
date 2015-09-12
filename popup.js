721304b52229754cc532f3fa0d3163c06febf53f
Harshvardhan Sharma <harsh1618@outlook.com>
2015-09-13 02:07:24 +0530

    find best matching ad title, cleanup

--- popup.js
+++ popup.js
@@ -1,81 +1,75 @@
-//function renderStatus(statusText) {
-//  document.getElementById('status').textContent = statusText;
-//}
-//
-//document.addEventListener('DOMContentLoaded', function() {
-//  getCurrentTabUrl(function(url) {
-//    // Put the image URL in Google search.
-//    renderStatus('Performing Google Image search for ' + url);
-//
-//    getImageUrl(url, function(imageUrl, width, height) {
-//
-//      renderStatus('Search term: ' + url + '\n' +
-//          'Google image search result: ' + imageUrl);
-//      var imageResult = document.getElementById('image-result');
-//      // Explicitly set the width/height to minimize the number of reflows. For
-//      // a single image, this does not matter, but if you're going to embed
-//      // multiple external images in your page, then the absence of width/height
-//      // attributes causes the popup to resize multiple times.
-//      imageResult.width = width;
-//      imageResult.height = height;
-//      imageResult.src = imageUrl;
-//      imageResult.hidden = false;
-//
-//    }, function(errorMessage) {
-//      renderStatus('Cannot display image. ' + errorMessage);
-//    });
-//  });
-//});
+var secret = "0c2487f6740c0bf89b64a4827e07cbe2";
 
-var token = '';
-var tokenId = '';
+var date = '2015-09-13';
+var appId = "541";
+var email = "mohitashokgarg@gmail.com";
 
 var productName = document.getElementsByTagName('h1')[0].innerHTML;
 var productCategory = getElementByXpath('//*[@id="fk-mainbody-id"]/div/div[2]/div/div/div/ul/li[2]/a').innerHTML;
+
+function numCommonWords(a, b) {
+    var lista = a.split(" ");
+    var listb = b.split(" ");
+    arrays = [lista, listb];
+    return arrays.shift().filter(function(v) {
+        return arrays.every(function(a) {
+            return a.indexOf(v) !== -1;
+        });
+    }).length;
+}
 
 function getElementByXpath(path) {
       return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
+function handleApiResponse() {
+    var response = JSON.parse(this.response);
+    var ads = response.AdsByCategoryResponse.AdsByCategoryData.docs;
+    var best = 0;
+    var maxScore = -1;
+    for (var i = 0; i < ads.length; i++) {
+        score = numCommonWords(productName, ads[i].title);
+        if (score > maxScore) {
+            maxScore = score;
+            best = i;
+        }
+        console.log(ads[i].title + " " + score);
+    }
+    console.log(ads[best].title);
+}
+
 function getApiResponse(apiName, apiParams, token, tokenId) {
     var apiReq = new XMLHttpRequest();
-    apiReq.open("GET", "https://api.quikr.com" + apiName + apiParams, false);
-    apiReq.setRequestHeader("X-Quikr-App-Id", "541");
+    apiReq.open("GET", "https://api.quikr.com" + apiName + apiParams, true);
+    apiReq.addEventListener("load", handleApiResponse);
+    apiReq.setRequestHeader("X-Quikr-App-Id", appId);
     apiReq.setRequestHeader("X-Quikr-Token-Id", tokenId);
 
-    var data = "541" + apiName + "2015-09-13";
+    var data = appId + apiName + date;
     var signature = CryptoJS.HmacSHA1(data, token).toString();
 
     apiReq.setRequestHeader("X-Quikr-Signature", signature);
     apiReq.send();
-    return apiReq.response;
 }
 
 function handleTokenResponse() {
     var response = JSON.parse(this.response);
-    token = response['token'];
-    tokenId = response['tokenId'];
-    console.log(token);
-    console.log(tokenId);
+    var token = response['token'];
+    var tokenId = response['tokenId'];
+
     var apiName = '/public/adsByCategory'; 
-    var apiParams = '?categoryId=149&from=0&size=20';
-    console.log(getApiResponse(apiName, apiParams, token, tokenId));
+    var apiParams = '?categoryId=149&from=0&size=200';
+
+    getApiResponse(apiName, apiParams, token, tokenId);
 }
 
-var secret = "0c2487f6740c0bf89b64a4827e07cbe2";
-
-var data = "mohitashokgarg@gmail.com" + "541" + "2015-09-13";
+var data = email + appId + date;
 var signature = CryptoJS.HmacSHA1(data, secret).toString();
-var tokenReqJson = {"appId" : "541", "signature" : signature};
+var tokenReqJson = {"appId" : appId, "signature" : signature};
 
 var tokenReq = new XMLHttpRequest();
 tokenReq.open("POST", "https://api.quikr.com/app/auth/access_token", true);
 tokenReq.addEventListener("load", handleTokenResponse);
 tokenReq.setRequestHeader("Content-Type", "application/json");
 tokenReq.send(JSON.stringify(tokenReqJson));
-
-//var categoryReq = new XMLHttpRequest();
-//categoryReq.open("GET", "https://api.quikr.com/public/adsByCategory?categoryId=149&from=0&size=20", true);
-//categoryReq.send();
-//console.log(categoryReq.responseText);
 
